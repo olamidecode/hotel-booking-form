c6460327b60ef93f3455d47a122db5d7b643f838
olamidecode <olaakinde16@gmail.com>
2019-09-11 14:39:19 +0200

    added a form and aded styling to the layout

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <!-- nav-bar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Akin's-Bookings</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -24,11 +24,11 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Contact</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+         Hotels
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Action</a>
@@ -51,16 +51,82 @@
   </div>
   <div class="card-body">
     <h5 class="card-title">Our moto and goals</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <p class="card-text">The harder you work for something, the greater you'll feel when you achieve it.</p>
     <a href="#" class="btn btn-primary">To the booking page</a>
   </div>
 </div>
-  
-</body>
+<section id="slide">
+<!-- slide show -->
+<!--Carousel Wrapper-->
+<div id="carousel-thumb" class="carousel slide carousel-fade carousel-thumbnails" data-ride="carousel">
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(88).jpg"
+        alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(121).jpg"
+        alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg"
+        alt="Third slide">
+    </div>
+  </div>
+  <!--/.Slides-->
+  <!--Controls-->
+  <a class="carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-thumb" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <!--/.Controls-->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-thumb" data-slide-to="0" class="active">
+      <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(88).jpg" width="100">
+    </li>
+    <li data-target="#carousel-thumb" data-slide-to="1">
+      <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(121).jpg" width="100">
+    </li>
+    <li data-target="#carousel-thumb" data-slide-to="2">
+      <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(31).jpg" width="100">
+    </li>
+  </ol>
+</div>
+</section>
+<article>
+  <div class="form">
+    <form>
+           <p> Enter your 1st name:</p>
+            <input type="text"  name="first name">
+            <br> 
+            <p>Enter your last name:</p>
+            <input type="text"  name="first name"> 
+            <br>
+            <p>Check in date:</p>
+            <input type="date" name="check in">
+            <br>
+            <p>Check out date:</p>
+            <input type="date" name="check out">
+            <br>
+            <input type="submit">
+            
+    </form>
+   </div>
+</article>
+<!--/.Carousel Wrapper-->
+<!-- ending of slide show -->
 <!-- bootstrap js and jquery -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <!-- main js -->
-<script src="main.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+
 </html>
