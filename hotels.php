06d90dd50f22bb2624a8d74688ddfbcd0dbd8819
olamidecode <olaakinde16@gmail.com>
2019-09-18 05:29:32 +0200

    fixed up my indentation

--- hotels.php
+++ hotels.php
@@ -21,20 +21,3 @@ END;
 
 }
 ?>
-<?php
-
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <!--Bootstrap CSS Stylesheet-->
-//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-//     <!--Custom StyleSheet-->
-//     <link href="css/style.css" rel="stylesheet" type="text/css">
-
-//     <title>Nav Hotel Booking</title>
-// </head>
-// <body class="form-body">
-?>
